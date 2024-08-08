abf517ad0b6ab28f425b0f12c0437e322c6aab02
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-08 23:39:16 +1200

    Modernize gem.

--- gems.rb
+++ gems.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2022, by Samuel Williams.
+# Copyright, 2022-2024, by Samuel Williams.
 
 source "https://rubygems.org"
 
@@ -15,6 +15,8 @@ end
 group :test do
 	gem "sus"
 	gem "covered"
+	gem "decode"
+	gem "rubocop"
 	
 	gem "bake-test"
 	gem "bake-test-external"
